095ca06c55bc92d32bbf96ae199adaf584d08d62
haris <haris.fadhilah21@gmail.com>
2023-03-14 14:05:03 +0700

    feat: setup uikit project programmatically

---
+++ MovieDb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDb
+//
+//  Created by Haris Fadhilah on 14/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
